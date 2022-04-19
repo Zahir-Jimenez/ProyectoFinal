76e2158c54640ff64edc2f831418487fb3e6ab70
davebeon <20151814@ce.pucmm.edu.do>
2022-04-19 03:21:35 -0400

    SubidaDHP

--- Projecto final/src/Logico/Clinica.java
+++ Projecto final/src/Logico/Clinica.java
@@ -1,10 +1,14 @@
 package Logico;
 
+import java.util.ArrayList;
+
 public class Clinica {
 	private ArrayList<Paciente> lospacientes;
 	private ArrayList<Persona> empleados;
 	private ArrayList<Consulta> reconsulta;
 	private ArrayList<Vacuna> revacunas;
 	private ArrayList<Enfermedad> enfermedades;
+	private ArrayList<Historial>miHistorial;
+	
 }
 
